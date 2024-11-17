75c2cbc6d4b8d4a35ca0261fd792c491b87ba630
SMIT-2411 <smit24@icloud.com>
2024-11-17 20:59:10 +0530

    reload done

--- Easy Browser/SceneDelegate.swift
+++ Easy Browser/SceneDelegate.swift
@@ -13,10 +13,17 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
 
 
     func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
-        // Use this method to optionally configure and attach the UIWindow `window` to the provided UIWindowScene `scene`.
-        // If using a storyboard, the `window` property will automatically be initialized and attached to the scene.
-        // This delegate does not imply the connecting scene or session are new (see `application:configurationForConnectingSceneSession` instead).
-        guard let _ = (scene as? UIWindowScene) else { return }
+        guard let windowScene = (scene as? UIWindowScene) else { return }
+        
+        window = UIWindow(frame: windowScene.coordinateSpace.bounds)
+        window?.windowScene = windowScene
+        // Initialize FirstScreen and wrap it in a UINavigationController
+        let firstScreen = ViewController()  // Assuming you have renamed ViewController to FirstScreen
+        let navigationController = UINavigationController(rootViewController: firstScreen)
+        
+        // Set the navigation controller as the root view controller
+        window?.rootViewController = navigationController
+        window?.makeKeyAndVisible()
     }
 
     func sceneDidDisconnect(_ scene: UIScene) {
--- Easy Browser/ViewController.swift
+++ Easy Browser/ViewController.swift
@@ -6,14 +6,87 @@
 //
 
 import UIKit
+import WebKit
 
-class ViewController: UIViewController {
 
+
+class ViewController: UIViewController, WKNavigationDelegate {
+    
+    // MARK: - Properties
+    private var webView: WKWebView!
+    private let initialURLString = "https://www.youtube.com/"
+    
+    // MARK: - Lifecycle Methods
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        navigationItem.rightBarButtonItem = UIBarButtonItem(title: "Open", style: .plain, target: self, action: #selector(openTapped))
+        
+        let spacer = UIBarButtonItem(barButtonSystemItem: .flexibleSpace, target: nil, action: nil)
+        let refresh = UIBarButtonItem(barButtonSystemItem: .refresh, target: webView, action: #selector(webView.reload))
+        
+        toolbarItems = [spacer, refresh,spacer]
+        navigationController?.isToolbarHidden = false
+        
+        
+        setupWebView()
+        loadInitialURL()
     }
-
-
+    
+    
+    @objc func openTapped(){
+        let ac = UIAlertController(title: "Open page...", message: nil, preferredStyle: .actionSheet)
+        ac.addAction(UIAlertAction(title: "apple.com", style: .default, handler: openPage))
+        ac.addAction(UIAlertAction(title: "youtube.com", style: .default, handler: openPage))
+        ac.addAction(UIAlertAction(title: "Cancel", style: .cancel))
+        
+        ac.popoverPresentationController?.barButtonItem = navigationItem.rightBarButtonItem
+        present(ac , animated: true)
+    }
+    
+    func openPage(action: UIAlertAction) {
+        let url = URL(string: "https://"+action.title!)
+        webView.load(URLRequest(url: url!))
+    }
+    
+    func webView(_ webView: WKWebView, didFinish navigation: WKNavigation!) {
+        title = webView.title
+    }
+    
+    
+    // MARK: - Web View Setup
+    private func setupWebView() {
+        // Initialize and configure the web view
+        webView = WKWebView()
+        webView.navigationDelegate = self
+        webView.translatesAutoresizingMaskIntoConstraints = false
+        
+        // Add the web view to the view hierarchy
+        view.addSubview(webView)
+        
+        // Set up Auto Layout constraints
+        NSLayoutConstraint.activate([
+            webView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor),
+            webView.leadingAnchor.constraint(equalTo: view.leadingAnchor),
+            webView.trailingAnchor.constraint(equalTo: view.trailingAnchor),
+            webView.bottomAnchor.constraint(equalTo: view.bottomAnchor)
+        ])
+    }
+    
+    // MARK: - Web View Configuration
+    private func loadInitialURL() {
+        // Safely unwrap the URL
+        guard let url = URL(string: initialURLString) else {
+            print("Invalid URL string: \(initialURLString)")
+            return
+        }
+        
+        // Create and load the URL request
+        let request = URLRequest(url: url)
+        webView.load(request)
+        
+        // Enable navigation gestures
+        webView.allowsBackForwardNavigationGestures = true
+    }
 }
 
